3503866addf0cc6881e5ea3c99a134c643174494
Ghost Boy <ericdepaulaapp@gmail.com>
2026-06-22 14:42:59 -0300

    Refactor atividade class attributes for encapsulation
    Updated attribute access levels in atividade class.

--- python/Atividade 10 - Diagramas Estrutural.py
+++ python/Atividade 10 - Diagramas Estrutural.py
@@ -1,14 +1,14 @@
 class atividade:
     def __init__(self, nome, descricao, data_entrega, nota):
         self.nome = nome
-        self.descricao = descricao
-        self.data_entrega = data_entrega
-        self.nota = nota
+        self._descricao = descricao
+        self._data_entrega = data_entrega
+        self.__nota = nota
 
 #função pra teste se os dados estão printando (dados ficticios.)
-minha_atv=atividade("Atividade Xtranha", "Isso é trap mixtape", "33/03/2033", 777-666)
+minha_atv=atividade("Atividade Xtranha", "Isso é trap mixtape", "33/03/2033", 777666)
 
 print(minha_atv.nome)
-print(minha_atv.descricao)
-print(minha_atv.data_entrega)
-print(minha_atv.nota)
+print(minha_atv._descricao)
+print(minha_atv._data_entrega)
+print(minha_atv.__nota)

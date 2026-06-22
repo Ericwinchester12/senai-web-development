02c4bb66cf2cf79056aadedbef00dde037bdd7f6
Ghost Boy <ericdepaulaapp@gmail.com>
2026-06-22 15:34:22 -0300

    Refactor atividade class attributes to private

--- python/Atividade 10 - Diagramas Estrutural.py
+++ python/Atividade 10 - Diagramas Estrutural.py
@@ -1,18 +1,18 @@
 class atividade:
     def __init__(self, nome, descricao, data_entrega, nota):
-        self.nome = nome
-        self.descricao = descricao
-        self.data_entrega = data_entrega
-        self.nota = nota
+        self._nome = nome
+        self._descricao = descricao
+        self._data_entrega = data_entrega
+        self.__nota = nota
 
     def exibir(self):
-#Exibe os dados da atividade
-        print(f"Nome: {self.nome}")
-        print(f"Descrição: {self.descricao}")
-        print(f"Data: {self.data_entrega}")
-        print(f"Nota: {self.nota}")
+        """Exibe os dados da atividade"""
+        print(f"Nome: {self._nome}")
+        print(f"Descrição: {self._descricao}")
+        print(f"Data: {self._data_entrega}")
+        print(f"Nota: {self.__nota}")
 
 #função pra teste se os dados estão printando (dados ficticios.)
-exibir_dados = atividade("Atividade Xtranha", "Isso é trap mixtape", "33/03/2033", 777-666)
+exibir_dados = atividade("Atividade Xtranha", "Isso é trap mixtape", "33/03/2033", "777-666")
 
 exibir_dados.exibir()
